ae7017cda3883ab63909eb92492717997fd9015b
Damien Redd <damiens.redd@gmail.com>
2023-02-27 09:46:10 -0600

    more solution

--- chal-build-tower/buildTower.js
+++ chal-build-tower/buildTower.js
@@ -5,8 +5,21 @@ const tower = (num) => {
     for (i = 0; i < num; i++) {
         let stars = math([i])
         let space = (math(num - 1) - stars) / 2
-        blocks.push(`${''.repeat(space)}${"*".repeat(stars)}${''.repeat(space)}`)
+        blocks.push(`${' '.repeat(space)}${"*".repeat(stars)}${' '.repeat(space)}`)
     }
     return blocks
 }
-console.log(tower(2))
+console.log(tower(15))
+
+console.log('==================================================================')
+
+function towerBuilder(nFloors) {
+    var tower = [];
+    for (var i = 0; i < nFloors; i++) {
+    tower.push(" ".repeat(nFloors - i - 1)
+    + "*".repeat((i * 2)+ 1)
+    + " ".repeat(nFloors - i - 1))
+    }
+    return tower;
+    }
+    console.log(towerBuilder(1))
